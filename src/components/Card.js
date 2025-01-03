69589b4cceb2d6a36edcb957a4845ca708816d92
Rusydina Novitasari <rnvitas697@gmail.com>
2025-01-03 21:38:43 +0700

    setup deploy

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,9 @@
 import "../css/benefits.css";
 
 function Card(props) {
-  // const DescrComponent = () => <p className="desc-benefits">{props.desc}</p>;
-
   return (
     <div className="card-benefits">
       <h4 className="title-benefits">{props.judul}</h4>
-      {/* <DescrComponent /> */}
       <p className="desc-benefits">{props.desc}</p>
     </div>
   );
